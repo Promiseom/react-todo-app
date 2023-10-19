e0f8c021d97492821562ba5fd7e420d2eaa8e4d4
Promise Anendah <anendahpromise@yahoo.com>
2023-10-19 15:13:00 +0100

    Add more functionality to todo app
    learnt how to use variables and props in react,
    learnt how to create and use react components,
    learnt how to add state to components using useState and hooks,
    learnt how to use prop-callbacks to pass information to component's parent,

--- src/App.js
+++ src/App.js
@@ -1,14 +1,67 @@
-function App(props) {
+import React, {useState} from "react";
+import Todo from "./components/Todo";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+import {nanoid} from "nanoid";
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello {props.subject}!
-        </p>
-      </header>
-    </div>
-  );
+function App(props) {
+	const [tasks, setTasks] = useState(props.tasks);
+
+	let addTask = (name)=>{
+		let newTask = {id: `todo-${nanoid()}`, name: name, completed: false};
+		setTasks([...tasks, newTask]);
+	};
+
+	let toggleTaskCompleted = (id)=>{
+		let updatedTasks = tasks?.map((task)=>{
+			if(task.id == id){
+				return {...task, completed: !task.completed};
+			}
+			return task;
+		});
+		setTasks(updatedTasks);
+	};
+
+	let deleteTask = (id)=>{
+		let remainingTasks = tasks.filter((task)=>task.id !== id);
+		setTasks(remainingTasks);
+	};
+
+	const taskList = tasks?.map((task) => (
+		<Todo 
+			id={task.id}
+			name={task.name} 
+			completed={task.completed} 
+			key={task.id}
+			toggleTaskCompleted = {toggleTaskCompleted}
+			deleteTask = {deleteTask}
+		/>
+	));	
+
+	const taskNoun = (taskList.length > 1 || taskList.length == 0)? 'tasks' : 'task';
+	const tasksHeading = `${taskList.length} ${taskNoun} remaining`;
+
+	return (
+		<div className="todoapp stack-large">
+			<h1>TodoMatic</h1>
+			<Form addTask={addTask}/>
+
+			<div className="filters btn-group stack-exception">
+				<FilterButton />
+				<FilterButton />
+				<FilterButton />
+			</div>
+			<h2 id="list-heading">{tasksHeading}</h2>
+			<ul
+				role="list"
+				className="todo-list stack-large stack-exception"
+				aria-labelledby="list-heading">
+				
+				{taskList}
+
+			</ul>
+		</div>
+	);
 }
 
 export default App;
