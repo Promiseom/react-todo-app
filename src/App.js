796c0c9b7b8860647a48149b74676d6e75819e90
Promise Anendah <anendahpromise@yahoo.com>
2023-10-20 03:12:10 +0100

    Add more functionality to the todo app
    finish implementing main features of app
    learn how to properly utilize javascript features in react development,

--- src/App.js
+++ src/App.js
@@ -4,8 +4,17 @@ import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import {nanoid} from "nanoid";
 
+const FILTER_MAP = {
+	All: () => true,
+	Active: (task) => !task.completed,
+	Completed: (task) => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
 	const [tasks, setTasks] = useState(props.tasks);
+	const [filter, setFilter] = useState("All");
 
 	let addTask = (name)=>{
 		let newTask = {id: `todo-${nanoid()}`, name: name, completed: false};
@@ -22,21 +31,41 @@ function App(props) {
 		setTasks(updatedTasks);
 	};
 
+	let editTask = (id, newName)=>{
+		let editedTasks = tasks?.map((task)=>{
+			if(task.id == id){
+				return {...task, name: newName};
+			}
+			return task;
+		});
+		setTasks(editedTasks);
+	};
+
 	let deleteTask = (id)=>{
 		let remainingTasks = tasks.filter((task)=>task.id !== id);
 		setTasks(remainingTasks);
 	};
 
-	const taskList = tasks?.map((task) => (
-		<Todo 
+	const taskList = tasks?.filter(FILTER_MAP[filter]).map((task) => (
+		<Todo
 			id={task.id}
-			name={task.name} 
-			completed={task.completed} 
+			name={task.name}
+			completed={task.completed}
 			key={task.id}
 			toggleTaskCompleted = {toggleTaskCompleted}
 			deleteTask = {deleteTask}
+			editTask = {editTask}
 		/>
 	));	
+
+	const filterList = FILTER_NAMES.map((name)=>(
+		<FilterButton
+			key={name}
+			name={name}
+			isPressed={name === filter}
+			setFilter={setFilter}
+		/>	
+	));
 
 	const taskNoun = (taskList.length > 1 || taskList.length == 0)? 'tasks' : 'task';
 	const tasksHeading = `${taskList.length} ${taskNoun} remaining`;
@@ -47,9 +76,7 @@ function App(props) {
 			<Form addTask={addTask}/>
 
 			<div className="filters btn-group stack-exception">
-				<FilterButton />
-				<FilterButton />
-				<FilterButton />
+				{filterList}
 			</div>
 			<h2 id="list-heading">{tasksHeading}</h2>
 			<ul
